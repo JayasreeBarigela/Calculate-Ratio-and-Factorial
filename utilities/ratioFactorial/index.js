2857276af2a5c21fa7b906a349aa4cc9aae5620a
JayasreeBarigela <96167672+JayasreeBarigela@users.noreply.github.com>
2022-01-27 21:18:34 +0530

    Update index.js

--- utilities/ratioFactorial/index.js
+++ utilities/ratioFactorial/index.js
@@ -1,11 +1,11 @@
 const factorial = require("../factorial/index");
 const ratio = require("../ratio/index");
 
-function ratioFactorial(a, b, c) {
+function ratioAndFactorial(a, b, c) {
   return {
     ratio: ratio(a, b),
     factorial: factorial(c),
   };
 }
 
-module.exports = ratioFactorial;
+module.exports = ratioAndFactorial;
